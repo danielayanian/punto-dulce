5cffbb5ab56a54d191cd50980ac2fdca11ab62d0
gabriela-ppv <gabrielapintovas@gmail.com>
2024-05-16 14:27:19 +0200

    services.2

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 // Fichero src/services/api.js
+
 const callToApi = () => {
   
    
@@ -27,22 +28,3 @@ export default callToApi;
 
 
 
-// Fichero src/services/api.js
-// const callToApi = () => {
-//     // Llamamos a la API
-//     return fetch("https://swapi.dev/api/people/5") // Este 5 es el id de Leia Skywalker
-//       .then((response) => response.json())
-//       .then((response) => {
-//         // Cuando responde la API podemos limpiar los datos aquí
-//         const result = {
-//           name: response.name,
-//           birthYear: response.birth_year,
-//           height: response.height,
-//           mass: response.mass,
-//           eyeColor: response.eye_color,
-//         };
-//         return result;
-//       });
-//   };
-  
-//   export default callToApi;
