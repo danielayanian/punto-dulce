090e66f2c39390b62896a7e16a84a8c37408fabf
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2024-05-19 20:45:19 +0200

    merged from gabi

---
+++ src/services/api.js
@@ -0,0 +1,30 @@
+// Fichero src/services/api.js
+
+const callToApi = () => {
+  
+   
+        return {
+         
+        };
+      }
+
+
+export default callToApi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
