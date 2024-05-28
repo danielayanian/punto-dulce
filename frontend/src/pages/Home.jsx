cb4092a7a2e032ae194b179b460102e4c40a0648
Altus24 <aldu.ing@gmail.com>
2024-05-28 12:10:09 -0300

    brand section done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,28 +2,30 @@ import Carousel from "../components/Carousel/Carousel";
 import Categories from "../components/Categories";
 import { GiftCard } from "../components/GiftCard/GiftCard";
 import Opinions from "../components/Opinions";
-
-
+import OtherBrands from "../components/OtherBrands/OtherBrands";
 
 export const Home = () => {
   const allCategories = [
-    { id: 1, categoryName: 'Alfajores', imageUrl: '../../../public/img/alfajores.png' },
-    
+    {
+      id: 1,
+      categoryName: "Alfajores",
+      imageUrl: "../../../public/img/alfajores.png",
+    },
   ];
   return (
     <>
-      <Carousel  />
+      <Carousel />
 
-      <Categories categories={allCategories}/>
+      <Categories categories={allCategories} />
 
       <h2>OFERTAS DE LA SEMANA</h2>
 
-      <Opinions/>
-  
-    <GiftCard/>
-      
+      <Opinions />
 
-      <div>ALGUNA DE NUESTRAS MARCAS</div>
+      <GiftCard />
+
+      <OtherBrands/>
+      
     </>
   );
 };
