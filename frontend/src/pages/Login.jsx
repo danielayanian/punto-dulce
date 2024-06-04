e05a45a39fecfdb72d9011626a7869762156b4c8
Lourdes69 <123782568+Lourdes69@users.noreply.github.com>
2024-06-04 11:29:37 -0300

    login y register terminado (#23)

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+
+//Login
+import React from 'react';
+import Input from '../components/LoginInput/LoginInput';
+
+export const LoginInput = () => {
+  return (
+    <div>
+   <Input/>
+   </div>
+  );
+};
+
+export default LoginInput;
