65a75d877e40d3225b6f1af83361755bbaab2e39
Lourdes Avalos <lulyavalosmartinez@gmail.com>
2024-06-04 16:23:19 -0300

    menu mobile terminado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.css';
+import MobileMenu from "../MobileMenu/MobileMenu"; 
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header className={styles.container}>
       
@@ -14,9 +21,12 @@ const Header = () => {
         <Link to="/cart" className={styles.icon} >
           <FontAwesomeIcon icon={faCartShopping} /> 
         </Link>
-        <Link to="/profile" className={styles.icon}>
-          <FontAwesomeIcon icon={faBars} /> 
-        </Link>
+
+        {/* Menu mobile */}
+         <div className={styles.icon} onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <MobileMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
       </nav>
     </header>
   );
