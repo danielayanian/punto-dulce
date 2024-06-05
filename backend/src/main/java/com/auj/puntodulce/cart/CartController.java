809e462e976dfc03a2d3c9dc81e78127bee6744f
Danny <dannym.mejia@gmail.com>
2024-06-05 09:01:21 +0200

    27-be-add-delete-cartproductid-endpoint

--- backend/src/main/java/com/auj/puntodulce/cart/CartController.java
+++ backend/src/main/java/com/auj/puntodulce/cart/CartController.java
@@ -2,7 +2,6 @@ package com.auj.puntodulce.cart;
 
 import com.auj.puntodulce.exception.CartNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -54,6 +53,21 @@ public class CartController {
             throw new CartNotFoundException("Cart not found");
         }
         return cartService.getCart(cartId);
+    }
+
+    @Operation(summary = "Delete a product from the cart", description = "Removes a product from the cart.", tags = {"cart"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully removed item from cart"),
+            @ApiResponse(responseCode = "404", description = "Cart not found or Product not found in cart"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @DeleteMapping("{productId}")
+    public CartDTO deleteProductFromCart(@PathVariable UUID productId, HttpServletRequest request) {
+        UUID cartId = getCartIdFromCookies(request);
+        if (cartId == null) {
+            throw new CartNotFoundException("Cart not found");
+        }
+        return cartService.removeItemFromCart(cartId, productId);
     }
 
     private UUID getCartIdFromCookies(HttpServletRequest request) {
