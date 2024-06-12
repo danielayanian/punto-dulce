606ca6813a63a5f528e3c40db59207ea950fc088
Danny <dannym.mejia@gmail.com>
2024-06-13 01:20:37 +0200

    fixed 400 message

--- backend/src/main/java/com/auj/puntodulce/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/auj/puntodulce/exception/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorMessage> handleIllegalArgumentException(IllegalArgumentException  ex) {
-        ErrorMessage errorMessage = new ErrorMessage("Invalid request\", \"Please check your request parameters.");
+        ErrorMessage errorMessage = new ErrorMessage("Invalid request, Please check your request parameters.");
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(CartNotFoundException.class)
